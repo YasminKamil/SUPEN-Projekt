550d5630930cbb47a043de36027f2fee7619734f
linneamalin <35684736+linneamalin@users.noreply.github.com>
2019-02-20 14:26:57 +0100

    + property

--- SUPEN-Projekt/Models/Booking.cs
+++ SUPEN-Projekt/Models/Booking.cs
@@ -14,5 +14,7 @@ namespace SUPEN_Projekt.Models {
 		public DateTime StartTime { get; set; }
 		public DateTime EndTime { get; set; }
 		public int Price { get; set; }
+        public string Hej { get; set; }
+}
 	}
 }
