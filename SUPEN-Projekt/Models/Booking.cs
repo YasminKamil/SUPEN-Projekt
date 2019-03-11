f475efb02721964711854a33aa8eda31ecf3c0fd
HenrikSchon <35686992+HenrikSchon@users.noreply.github.com>
2019-03-11 11:40:19 +0100

    - branch

--- SUPEN-Projekt/Models/Booking.cs
+++ SUPEN-Projekt/Models/Booking.cs
@@ -15,6 +15,6 @@ namespace SUPEN_Projekt.Models {
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public int Price { get; set; }
-        public List<Branch> branches = new List<Branch>();
+
     }
 }
