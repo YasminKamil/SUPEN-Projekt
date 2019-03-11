8db024a97d58ebd67823680581710e3dd984ad0a
YasminKamil <yasminakamil@gmail.com>
2019-03-11 11:33:37 +0100

    Api för BookingSystem, funkar inte när man försöker köra det och få upp det som XML

---
+++ SUPEN-Projekt/Controllers/BookingSystemApiController.cs
@@ -0,0 +1,73 @@
+﻿using SUPEN_Projekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SUPEN_Projekt.Controllers
+{
+    public class BookingSystemApiController : ApiController{
+		private readonly ApplicationDbContext db;
+
+		public BookingSystemApiController() {
+			db = new ApplicationDbContext();
+		}
+
+		public IEnumerable<BookingSystem> GetBookingSystems() {
+			return db.BookingSystem.ToList();
+		}
+
+		[HttpPost]
+		public BookingSystem CreateBookingSystem(BookingSystem system) {
+			if (!ModelState.IsValid) {
+				throw new HttpResponseException(HttpStatusCode.BadRequest);
+			}
+			db.BookingSystem.Add(system);
+			db.SaveChanges();
+
+			return system;
+		}
+
+		public void UpdateBookingSystem (int id, BookingSystem system) {
+			if (!ModelState.IsValid) {
+				throw new HttpResponseException(HttpStatusCode.BadRequest);
+			}
+
+			var bookingSystem = db.BookingSystem.SingleOrDefault(x => x.BookingSystemId == id);
+			if(bookingSystem == null) {
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+			}
+
+			bookingSystem.Address = system.Address;
+			bookingSystem.City = system.City;
+			bookingSystem.CompanyName = system.CompanyName;
+			bookingSystem.ContactEmail = system.ContactEmail;
+			bookingSystem.ContactPhone = system.ContactPhone;
+			bookingSystem.Email = system.Email;
+			bookingSystem.Latitude = system.Latitude;
+			bookingSystem.Longitude = system.Longitude;
+			bookingSystem.PhoneNumber = system.PhoneNumber;
+			bookingSystem.PostalCode = system.PostalCode;
+			bookingSystem.SystemDescription = system.SystemDescription;
+			bookingSystem.SystemName = system.SystemName;
+			bookingSystem.Website = system.Website;
+
+			db.SaveChanges();
+		}
+
+		[HttpDelete]
+		public void DeleteBookingSystem(int id) {
+			var system = db.BookingSystem.SingleOrDefault(x => x.BookingSystemId == id);
+
+			if(system == null) {
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+			}
+
+			db.BookingSystem.Remove(system);
+			db.SaveChanges();
+		}
+
+    }
+}
