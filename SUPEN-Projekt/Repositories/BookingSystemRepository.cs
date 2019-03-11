30404f0fd521a370ff66449776867d4ffffa01eb
linneamalin <35684736+linneamalin@users.noreply.github.com>
2019-03-11 13:05:22 +0100

    IBookingSystemRepository BookingRepository

---
+++ SUPEN-Projekt/Repositories/BookingSystemRepository.cs
@@ -0,0 +1,25 @@
+﻿using SUPEN_Projekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SUPEN_Projekt.Repositories
+{
+    public class BookingSystemRepository : Repository<BookingSystem>, IBookingSystemRepository
+    {
+        public BookingSystemRepository(ApplicationDbContext context) : base(context)
+        { }
+
+        public IEnumerable<BookingSystem> GetAllBookingSystems()
+        {
+            return ApplicationDbContext.Set<BookingSystem>().ToList();
+        }
+
+        public ApplicationDbContext ApplicationDbContext
+        {
+            get { return Context as ApplicationDbContext; }
+        }
+    }
+}
+    
---
+++ SUPEN-Projekt/Repositories/IBookingSystemRepository.cs
@@ -0,0 +1,17 @@
+﻿using SUPEN_Projekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUPEN_Projekt.Repositories
+{
+    interface IBookingSystemRepository : IRepository<BookingSystem>
+    {
+
+        IEnumerable<BookingSystem> GetAllBookingSystems();
+
+    }
+    
+}
