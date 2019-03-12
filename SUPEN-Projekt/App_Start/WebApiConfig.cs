7766c0241cdae4596294bde9f233709d400f01ac
HenrikSchon <35686992+HenrikSchon@users.noreply.github.com>
2019-03-12 15:52:34 +0100

    + action i api

--- SUPEN-Projekt/App_Start/WebApiConfig.cs
+++ SUPEN-Projekt/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace SUPEN_Projekt
             config.MapHttpAttributeRoutes();
 				config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
