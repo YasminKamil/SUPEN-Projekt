a6f02256de47a13f648bf655aca7adf4241649d5
linneamalin <35684736+linneamalin@users.noreply.github.com>
2019-03-28 12:42:19 +0100

    vyer - pågående

---
+++ SUPEN-Projekt/Controllers/ServiceController.cs
@@ -0,0 +1,99 @@
+﻿using SUPEN_Projekt.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SUPEN_Projekt.Controllers
+{
+    public class ServiceController : Controller
+    {
+        UnitOfWork uw;
+
+        public ServiceController(UnitOfWork unityOfWork)
+        {
+            uw = unityOfWork;
+        }
+
+        // GET: Service
+        public ActionResult Index(int id)
+        {
+            //returnerar en specifik service
+            //return View(GetTheService(id));
+            return View();
+        }
+
+        // GET: Service/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Service/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Service/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Service/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Service/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Service/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Service/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
