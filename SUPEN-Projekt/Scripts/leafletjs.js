4af70dbef241cd4c7fcab71b129ff43aa878b63b
linneamalin <35684736+linneamalin@users.noreply.github.com>
2019-05-02 15:45:30 +0200

    små ändringar i css och en stylessheet för vardera details-vy och relevantbooking-vy

--- SUPEN-Projekt/Scripts/leafletjs.js
+++ SUPEN-Projekt/Scripts/leafletjs.js
@@ -1,73 +1,33 @@
-﻿
-//        mapboxgl.accessToken = 'pk.eyJ1IjoibmVociIsImEiOiJjanV4eGg1NWgwZjBsNDVtMjNqOGFlMjN2In0.6LFtJumVPCc9ncOf5JG4_A';
-//var map = new mapboxgl.Map({
-//            container: 'mapid',
-//        style: 'mapbox://styles/mapbox/streets-v11'
-//        });
+﻿var map = L.map('mapid', {
+    zoomControl: false
+}).setView([parseFloat(latitude), parseFloat(longitude)], 14);
 
 
-
-//var marker = L.marker([59.26153, 15.20781]).addTo(mymap);
-
-
-
-//var ports = $.ajax({
-//    url: "http://localhost:55341/api/GetBooking/",
-//    dataType: "json",
-//    success: console.log("County data successfully loaded."),
-//})
-
-//var inBookingSystemId = document.getElementById('mapid').value;
-//var inServiceId = document.childNodes.item.getElementById('inServiceId').value;
-//var inBookingId = document.childNodes.item.getElementById('inBookingId').value;
-
-var ports = $.ajax({
-    url: '@Url.Action("Details", "Booking", new { inBookingSystemId = Model.bookingSystem.BookingSystemId, inServiceId = Model.service.ServiceId, inBookingId = Model.booking.BookingId })',
-   type: 'GET',
-    dataType: "json",
-    success: function (mymap) {
-        successful(mymap);
-    }
-
-})
-
-var geojson = {
-    type: "FeatureCollection",
-    features: [],
-};
-
-for (var i in ports.data) {
-    for (var j in i) {
-        if (j.includes(inBookingSystemId))
-            
-                geojson.features.push({
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [i[j].longitude, i[j].latitude]
-                    },
-                    "properties": {
-                        "stationName": ports.data[i].port_name
-                    }
-                });        
-       
-    }
-}
-
-var mymap = L.map('mapid').setView([59.27894, 15.21134], 13);
-
+L.control.zoom({
+    position: 'bottomright'
+}).addTo(map);
 
 L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 18,
-    subdomains: ['a', 'c', 'd'],
-    //    //id: 'mapbox.streets', ?access_token={accessToken}
-    //    //accessToken: 'pk.eyJ1IjoiaGVua2UiLCJhIjoiY2p1djFnMWJjMGdhdjQzbm9vODZ6bXBxOCJ9.dqk32dR_wLAotleAp15vfw'
-}).addTo(mymap);
-L.Marker(L.geoJSON(geojson)).addTo(mymap);
-function successful(mymap) {
-    $("mapid").geoJSON(mymap).replaceWith;
+    subdomains: ['a', 'b', 'c']
+}).addTo(map);
+
+L.marker([parseFloat(latitude), parseFloat(longitude)]).addTo(map);
+
+var popup = L.popup()
+    .setLatLng([parseFloat(latitude), parseFloat(longitude)])
+            .setContent("companyName")
+    .openOn(map);
+
+var markers = [{ "lat":latitude , "long": longitude}];
+
+
+
+for (var j = 0; j < markers.length; j++) {
+
+    L.marker([parseFloat(markers[j].lat), parseFloat(markers[j].long)]).addTo(map);
+
 }
-   
 
 
