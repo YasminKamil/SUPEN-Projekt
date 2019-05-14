25d74b9a5db0aee9e3cb46f184f36079d73e5936
YasminKamil <yasminakamil@gmail.com>
2019-05-14 10:52:11 +0200

    + kommentarer, indentering och städning av kod i ApiBookingController

--- SUPEN-Projekt/API/ApiBookingController.cs
+++ SUPEN-Projekt/API/ApiBookingController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SUPEN_Projekt.Controllers {
 	public class ApiBookingController : ApiController {
 
-		//IUnitOfWork följer indpendancy injection. Kommunicerar med repository interfaces
+		//IUnitOfWork följer depedency injection. Kommunicerar med repository interfaces
 		//Se Repository UnitOfWork för implementation
 		IUnitOfWork uw;
 
@@ -15,10 +15,11 @@ namespace SUPEN_Projekt.Controllers {
 			uw = unitOfWork;
 		}
 
-		//Hämtar alla lagrade bokningar
+		//Returnerar alla lagrade bokningar som finns i databasen
 		[Route("api/GetBookings")]
 		[HttpGet]
 		public async Task<IHttpActionResult> GetBookings() {
+			//Hämtar bokningar genom att anropa på GetAll() metoden som används i repository
 			var bookings = await uw.Bookings.GetAll();
 			BookingsViewModel list = new BookingsViewModel();
 			list.bookings = bookings;
@@ -28,6 +29,7 @@ namespace SUPEN_Projekt.Controllers {
 			return Ok(list);
 		}
 
+		//Returnerar en specifik bokning som finns lagrad i databasen
 		[Route("api/GetBooking/{inBookingId}")]
 		[HttpGet]
 		public async Task<IHttpActionResult> GetBooking(int inBookingId) {
@@ -39,19 +41,20 @@ namespace SUPEN_Projekt.Controllers {
 			if (bsSBVM == null) {
 				return NotFound();
 			}
-
 			return Ok(bsSBVM);
 		}
 
+		//Returnerar en specifik bokning som tillhör en specifik tjänst som finns lagrad i databasen
 		[Route("api/GetBooking/{inBookingSystemId}/{inServiceId}")]
 		[HttpGet]
 		public async Task<IHttpActionResult> GetBooking(int inBookingSystemId, int inServiceId) {
+			//Hämtar bokningssystemet genom att anropa GetBookingSystem() metoden från repository
 			var bs = await uw.BookingSystems.GetBookingSystem(inBookingSystemId);
 			BookingSystemServiceBookingViewModel bsSBVM = new BookingSystemServiceBookingViewModel();
 			bsSBVM.bookingSystem = bs;
+
+			//Hämtar den specifika tjänsten som tillhör bokningssystemet
 			bsSBVM.service = bs.Services.Single(x => x.ServiceId == inServiceId);
-			//bsSBVM.booking = bsSBVM.service.Bookings.Single(x => x.BookingId == inBookingId);
-			//bsSBVM.booking = uw.Bookings.GetBookings().Single(x=>x.BookingId == inBookingId);
 
 			if (bsSBVM == null) {
 				return NotFound();
@@ -59,6 +62,7 @@ namespace SUPEN_Projekt.Controllers {
 			return Ok(bsSBVM);
 		}
 
+		//Hämtar det högsta id:et för bokningar som finns lagrad i databasen
 		[Route("api/GetBooking/GetMaxId")]
 		[HttpGet]
 		public async Task<IHttpActionResult> GetMaxId() {
@@ -68,6 +72,7 @@ namespace SUPEN_Projekt.Controllers {
 			return Ok(model);
 		}
 
+		//Skapar en ny post av en bokning som lagras i databasen
 		[Route("api/PostBooking")]
 		[HttpPost]
 		public async Task<IHttpActionResult> PostBooking(BookingSystemServiceBookingViewModel inBooking) {
@@ -76,21 +81,17 @@ namespace SUPEN_Projekt.Controllers {
 			}
 
 			var booking = inBooking.booking;
-
 			booking.StartTime = inBooking.startTime;
 			booking.EndTime = inBooking.startTime.AddMinutes(inBooking.service.Duration);
 
+			/*Gör först ett anrop till AddBooking i repository som kräver en bokning och service id som parameter.
+			 Tar in CreateBooking() metoden från repository som första parametervärde för bokningen och sedan service objektet
+			 som andra parametervärde*/
 			uw.Services.AddBooking(await uw.Bookings.CreateBooking(booking), inBooking.service.ServiceId);
 			uw.Complete();
 
 			return Ok();
 		}
-
 	}
 }
-
-
-
-
-
 
