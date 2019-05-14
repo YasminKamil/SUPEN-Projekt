cfdff9ace0d886cec846f2a46093cc9e99042761
YasminKamil <yasminakamil@gmail.com>
2019-05-14 11:41:57 +0200

    + kommentarer, indentering och städning av kod i ApiService

--- SUPEN-Projekt/API/ApiServiceController.cs
+++ SUPEN-Projekt/API/ApiServiceController.cs
@@ -6,38 +6,39 @@ using System.Threading.Tasks;
 
 namespace SUPEN_Projekt.Controllers {
 	public class ApiServiceController : ApiController {
+		//IUnitOfWork följer depedency injection. Kommunicerar med repository interfaces
+		//Se Repository UnitOfWork för implementation
 		IUnitOfWork uw;
 
 		public ApiServiceController(IUnitOfWork unitOfWork) {
 			uw = unitOfWork;
 		}
 
-		//Hämtar alla lagrade tjänster 
+		//Returnerar alla lagrade tjänster som finns i databasen
 		[Route("api/GetServices")]
 		[HttpGet]
 		public async Task<IHttpActionResult> GetServices() {
 			var services = await uw.Services.GetAll();
 			ServicesViewModel list = new ServicesViewModel();
 
-
 			list.services = services.ToList();
 
 			if (list == null) {
 				return NotFound();
 			}
-
 			return Ok(list);
 		}
 
-		//Hämtar den specifika tjänsten i bokningsystemet som är lagrad
+		//Returnerar en specifik tjänst som finns lagrad i databasen
 		[Route("api/GetService/{inBookingSystemId}/{inServiceId}")]
 		[HttpGet]
 		public async Task<IHttpActionResult> GetService(int inBookingSystemId, int inServiceId) {
-
+			//Anropar på GetBookingSystem() från repository för att hämta det specifika bokningssystemet
 			var bs = await uw.BookingSystems.GetBookingSystem(inBookingSystemId);
 			BookingSystemServiceBookingViewModel bsSBVM = new BookingSystemServiceBookingViewModel();
 
 			bsSBVM.bookingSystem = bs;
+			//Tilldelar service-objektet ett värde på en tjänst som tillhör ett specifikt bokningssystem
 			bsSBVM.service = bsSBVM.bookingSystem.Services.Single(x => x.ServiceId == inServiceId);
 
 			if (bsSBVM == null) {
@@ -47,32 +48,35 @@ namespace SUPEN_Projekt.Controllers {
 			return Ok(bsSBVM);
 		}
 
+		//Returnerar förslag på tjänster som användaren har ofta bokat som finns lagrad i databasen
 		[Route("api/GetService/{inBookingId}/{inServiceName}/{inBookingSystemId}")]
 		[HttpGet]
 		public async Task<IHttpActionResult> GetServiceSuggestion(int inBookingId, string inServiceName, int inBookingSystemId) {
-			ServiceViewModel serviceViewModel = new ServiceViewModel();
+			ServiceViewModel sVM = new ServiceViewModel();
 
 			var inBooking = await uw.Bookings.Get(inBookingId);
 			var bookingSystems = await uw.BookingSystems.GetAll();
 
-            var bookingSystem = await uw.BookingSystems.GetBookServiceSuggestion(inBooking, inServiceName, inBookingSystemId);
-            var service = await uw.Services.GetServiceSuggestion(bookingSystem);
-            var booking = await uw.BookingSystems.GetServiceSuggestionBookings(bookingSystems.ToList(), inBooking);
-            
-            serviceViewModel.bookingSystemName = bookingSystem.CompanyName;
-            serviceViewModel.serviceName = service.ServiceName;
-            serviceViewModel.startTime = booking.StartTime;
-            serviceViewModel.endTime = booking.EndTime;
-            serviceViewModel.bookingSystemId = bookingSystem.BookingSystemId;
-            serviceViewModel.serviceId = service.ServiceId;
-            serviceViewModel.branchAId = service.Branch.BranchId;
-            serviceViewModel.bookingId = booking.BookingId;
-            serviceViewModel.PictureUrl = service.Branch.PictureUrl;
-            if (serviceViewModel == null) {
+			//Tilldelar respektive variabel ett värde genom att anropa på metoder från repository
+			var bookingSystem = await uw.BookingSystems.GetBookServiceSuggestion(inBooking, inServiceName, inBookingSystemId);
+			var service = await uw.Services.GetServiceSuggestion(bookingSystem);
+			var booking = await uw.BookingSystems.GetServiceSuggestionBookings(bookingSystems.ToList(), inBooking);
+
+			sVM.bookingSystemName = bookingSystem.CompanyName;
+			sVM.serviceName = service.ServiceName;
+			sVM.startTime = booking.StartTime;
+			sVM.endTime = booking.EndTime;
+			sVM.bookingSystemId = bookingSystem.BookingSystemId;
+			sVM.serviceId = service.ServiceId;
+			sVM.branchAId = service.Branch.BranchId;
+			sVM.bookingId = booking.BookingId;
+			sVM.PictureUrl = service.Branch.PictureUrl;
+
+			if (sVM == null) {
 				return NotFound();
 			}
 
-			return Ok(serviceViewModel);
+			return Ok(sVM);
 		}
 	}
 }
