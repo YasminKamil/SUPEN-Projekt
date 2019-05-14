dbcf77dee34019e3f9cc47b29fced5d6b0c3004d
YasminKamil <yasminakamil@gmail.com>
2019-05-14 09:15:21 +0200

    - tagit bort bookingsystemqueryviewmodel

--- SUPEN-Projekt/Logic/ViewModels/BookingSystemQueryViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SUPEN_Projekt.Logic.ViewModels {
-	public class BookingSystemQueryViewModel {
-		public int firstId { get; set; }
-		public int secondId { get; set; }
-		public int thirdId { get; set; }
-	}
-}
