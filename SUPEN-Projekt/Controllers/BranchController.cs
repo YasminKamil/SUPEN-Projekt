c02e4880e9f408b9ae4c36811ed72b454f11252f
YasminKamil <yasminakamil@gmail.com>
2019-05-14 10:22:42 +0200

    + kommentarer och indentering i BranchController

--- SUPEN-Projekt/Controllers/BranchController.cs
+++ SUPEN-Projekt/Controllers/BranchController.cs
@@ -7,73 +7,54 @@ using System.Threading.Tasks;
 using System.Web.Mvc;
 using SUPEN_Projekt.Logic.ViewModels;
 
-namespace SUPEN_Projekt.Controllers
-{
-    public class BranchController : Controller
-    {
+namespace SUPEN_Projekt.Controllers {
+	public class BranchController : Controller {
 
+		//Returnerar en branch
+		public async Task<ActionResult> Branch(int inBranchId) {
+			BranchViewModel bVM = null;
+			HttpClient client = new HttpClient();
 
+			//Gör ett api-anrop för att hämta en bransch
+			var result = client.GetAsync("http://localhost:55341/api/GetBranch/" + inBranchId).Result;
+			if (result.IsSuccessStatusCode) {
+				bVM = await result.Content.ReadAsAsync<BranchViewModel>();
+			} else {
+				ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+			}
+			return View(bVM);
+		}
 
-        //Returnerar det valda bokningsystemets tjänster
-        public async Task<ActionResult> Branch(int inBranchId)
-        {
-            BranchViewModel bVM = null;
-            HttpClient client = new HttpClient();
-            var result = client.GetAsync("http://localhost:55341/api/GetBranch/" + inBranchId).Result;
-            if (result.IsSuccessStatusCode)
-            {
-                bVM = await result.Content.ReadAsAsync<BranchViewModel>();
-            }
-            else
-            {
-                ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-            }
-            return View(bVM);
-        }
+		//Returnerar branscher genom att göra ett api-anrop
+		public async Task<ActionResult> Branches() {
+			BranchesViewModel bVM = null;
+			HttpClient client = new HttpClient();
 
-        //Returnerar det valda bokningsystemets tjänster
-        public async Task<ActionResult> Branches()
-        {
-            BranchesViewModel bVM = null;
-            HttpClient client = new HttpClient();
-            var result = client.GetAsync("http://localhost:55341/api/GetBranches/").Result;
-            if (result.IsSuccessStatusCode)
-            {
-                bVM = await result.Content.ReadAsAsync<BranchesViewModel>();
-            }
-            else
-            {
-                ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-            }
-            return View(bVM);
-        }
+			//Gör ett api-anrop för att hämta flera branscher
+			var result = client.GetAsync("http://localhost:55341/api/GetBranches/").Result;
+			if (result.IsSuccessStatusCode) {
+				bVM = await result.Content.ReadAsAsync<BranchesViewModel>();
+			} else {
+				ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+			}
+			return View(bVM);
+		}
 
+		/*Metoden används för att göra interna metodanrop, som tar in url:en som används för att kontrollera url:en som används
+		  för att göra api-anrop och objekten som ska tas in i api-anropet*/
+		public async Task<bool> APIContact(string inUrl, Object inObject) {
+			bool works = false;
+			var url = inUrl;
+			using (var client = new HttpClient()) {
 
-        public async Task<bool> APIContact(string inUrl, Object inObject)
-        {
-            bool works = false;
-            var url = inUrl;
-            using (var client = new HttpClient())
-            {
-                var content = new StringContent(JsonConvert.SerializeObject(inObject), Encoding.UTF8, "application/json");
-                var result = await client.PostAsync(url, content);
-                if (result.IsSuccessStatusCode)
-                {
-                    works = true;
-                }
-            }
-            return works;
-        }
-
-
-
-
-
-
-
-
-
-
-
-    }
+				//Serialiserar objekten som tas in till JSON-objekt
+				var content = new StringContent(JsonConvert.SerializeObject(inObject), Encoding.UTF8, "application/json");
+				var result = await client.PostAsync(url, content);
+				if (result.IsSuccessStatusCode) {
+					works = true;
+				}
+			}
+			return works;
+		}
+	}
 }
