87aa6d1b50e1f66d9c8451d14a26d100ba944b0c
linneamalin <35684736+linneamalin@users.noreply.github.com>
2022-04-15 02:05:45 +0200

    updates

--- SUPEN-Projekt/App_Start/BundleConfig.cs
+++ SUPEN-Projekt/App_Start/BundleConfig.cs
@@ -16,8 +16,10 @@ namespace SUPEN_Projekt {
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+           
+
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
